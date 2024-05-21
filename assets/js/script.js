c884b9d8b0d178e49811053537cc55d1fab0d0f8
Lewis Mayhew <lewis_mayhew@hotmail.co.uk>
2024-05-21 15:10:58 +0000

    Add initital functions for hide and display sections

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+/* Welcome screen functions */
+// Hide welcome screen on 'START' button click
+function hideWelcome() {
+
+}
+// Display welcome screen before game start and when Play Again clicked
+function displayWelcome() {
+
+}
+
+/* Question screen functions */
+// Hide questions before quiz start and after quiz finished
+function hideQuestions() {
+
+}
+// Display questions while quiz is active
+function showQuestions() {
+
+}
+
+/* Final Score screen functions */
+// Hide the final score screen until quiz complete
+function hideFinalScore() {
+
+}
+// Display final score screen when quiz complete
+function displayFinalScore() {
+
+}
